bd7daff133a9767960d59048e7f8ba01c9f1f02b
Shivek Khurana <khuranashivek@gmail.com>
2014-04-26 14:04:26 +0530

    Sensor data at 100 hz done.

--- app/src/main/java/com/bluetoothclient/ConnectedActivity.java
+++ app/src/main/java/com/bluetoothclient/ConnectedActivity.java
@@ -14,10 +14,7 @@ import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.view.*;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
@@ -41,15 +38,11 @@ public class ConnectedActivity extends ActionBarActivity implements SensorEventL
     private final  BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     private BluetoothDevice mBluetoothDevice;
     private String mac_address = null;
-    private ConnectThread mConnectThread;
-    private ConnectedThread mConnectedThread = null;
-    private BluetoothConnector mBluetoothConnector;
     private ConnectToDevice connectToDevice;
     private StreamToServer streamToServer;
     private SensorManager sensorManager;
     private Sensor accelrometer;
-    public static final int BOND_BONDED = 12;
-
+    private int t=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,8 +61,8 @@ public class ConnectedActivity extends ActionBarActivity implements SensorEventL
         TextView deviceName = (TextView) findViewById(R.id.deviceName);
         deviceName.setText(mBluetoothDevice.getName());
 
-        EditText chatMessage = (EditText) findViewById(R.id.chatMessage);
-        chatMessage.setOnEditorActionListener(onSendChatMessage);
+        Button closeConnection = (Button) findViewById(R.id.closeConnection);
+        closeConnection.setOnClickListener(onCloseConnectionClick);
 
         candidateUUIDs.add(UUID.fromString("a1a738e0-c3b3-11e3-9c1a-0800200c9a66"));
         candidateUUIDs.add(UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee"));
@@ -89,27 +82,24 @@ public class ConnectedActivity extends ActionBarActivity implements SensorEventL
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelrometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        /*mConnectThread = new ConnectThread(mBluetoothDevice);
-        mConnectThread.start();*/
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 
-    TextView.OnEditorActionListener onSendChatMessage = new TextView.OnEditorActionListener() {
+    View.OnClickListener onCloseConnectionClick = new View.OnClickListener() {
         @Override
-        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-            if(actionId == EditorInfo.IME_ACTION_SEND){
-                String c = v.getText().toString();
-                v.setText("");
-                mConnectedThread.write(c.getBytes());
-            }
-            return false;
+        public void onClick(View v) {
+            streamToServer.write("quit".getBytes());
+            Intent intent = new Intent(ConnectedActivity.this, MainActivity.class);
+            startActivity(intent);
         }
     };
 
     @Override
     public final void onSensorChanged(SensorEvent event) {
-        byte[] b = String.valueOf(event.timestamp+','+event.values[0]+','+event.values[1]+','+event.values[2]).getBytes();
+        t = t+1;
+        byte[] b = (String.valueOf(t) + "," +String.valueOf(event.values[0]) + "," + String.valueOf(event.values[1]) + "," + String.valueOf(event.values[2])).getBytes();
         streamToServer.write(b);
-        //Log.d(TAG, "Got sensor event" + String.valueOf(event.timestamp)+ " : " + event.values[0]);
     }
 
     @Override
@@ -120,7 +110,7 @@ public class ConnectedActivity extends ActionBarActivity implements SensorEventL
     @Override
     protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(this, accelrometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, accelrometer, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
@@ -149,134 +139,6 @@ public class ConnectedActivity extends ActionBarActivity implements SensorEventL
         return super.onOptionsItemSelected(item);
     }
 
-    private class ConnectThread extends Thread {
-        private final BluetoothSocket mmSocket;
-        private final BluetoothDevice mmDevice;
-        public static final int BOND_BONDING = 11;
-        public static final int BOND_BONDED = 12;
-
-        private int bondState;
-
-        public ConnectThread(BluetoothDevice device) {
-            // Use a temporary object that is later assigned to mmSocket,
-            // because mmSocket is final
-            BluetoothSocket tmp = null;
-            mmDevice = device;
-
-            // Get a BluetoothSocket to connect with the given BluetoothDevice
-            try {
-                // MY_UUID is the app's UUID string, also used by the server code
-                tmp = device.createRfcommSocketToServiceRecord(UUID.fromString("11"));
-            } catch (IOException e) {
-
-            }
-            mmSocket = tmp;
-        }
-
-        public void run() {
-            // Cancel discovery because it will slow down the connection
-            mBluetoothAdapter.cancelDiscovery();
-
-            Log.d(TAG, "Entering socket connect loop");
-            while(true){
-                try {
-                    mmSocket.connect();
-                    break;
-                } catch (IOException connectException){
-                    Log.d(TAG, "Unable to connect, IOException, Retrying");
-                }
-            }
-            Log.d(TAG, "Socket Connected");
-
-            while(true) {
-                bondState = mmDevice.getBondState();
-                if (bondState == BOND_BONDING) {
-                    Log.d(TAG, "Bonding in process");
-                } else if (bondState == BOND_BONDED) {
-                    Log.d(TAG, "Bond complete");
-                    break;
-                }
-            }
-            manageConnectedSocket(mmSocket);
-        }
-
-        /** Will cancel an in-progress connection, and close the socket */
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) { }
-        }
-    }
-    private void manageConnectedSocket(BluetoothSocket mSocket){
-        mConnectedThread = new ConnectedThread(mSocket);
-
-        while(true){
-            if(mBluetoothDevice.getBondState() == BOND_BONDED){
-                break;
-            }
-        }
-        Log.d(TAG, "Paired. You may now kiss the bride.");
-        mConnectedThread.start();
-    }
-    private class ConnectedThread extends Thread {
-        private final BluetoothSocket mmSocket;
-        private final InputStream mmInStream;
-        private final OutputStream mmOutStream;
-
-        public ConnectedThread(BluetoothSocket socket) {
-            mmSocket = socket;
-            InputStream tmpIn = null;
-            OutputStream tmpOut = null;
-
-            // Get the input and output streams, using temp objects because
-            // member streams are final
-            try {
-                tmpIn = socket.getInputStream();
-                tmpOut = socket.getOutputStream();
-            } catch (IOException e) { }
-
-            mmInStream = tmpIn;
-            mmOutStream = tmpOut;
-        }
-
-        public void run() {
-            byte[] buffer = new byte[1024];  // buffer store for the stream
-            //int bytes; // bytes returned from read()
-
-            Log.d(TAG, "ConnectedThread @run activated");
-
-            // Keep listening to the InputStream until an exception occurs
-            while (true) {
-                try {
-                    // Read from the InputStream
-                    //bytes = mmInStream.read(buffer);
-                    // Send the obtained bytes to the UI activity
-                    //mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer).sendToTarget();
-                    //Toast.makeText(MainActivity.this, new String(buffer, "UTF-8"), Toast.LENGTH_LONG).show();
-                    String chatReply = new String(buffer, "UTF-8");
-                    Log.i(TAG, "Received : " + chatReply);
-
-                } catch (IOException e) {
-                    break;
-                }
-            }
-        }
-
-        /* Call this from the main activity to send data to the remote device */
-        public void write(byte[] bytes) {
-            try {
-                mmOutStream.write(bytes);
-            } catch (IOException e) { }
-        }
-
-        /* Call this from the main activity to shutdown the connection */
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) { }
-        }
-    }
-
     private class ConnectToDevice extends Thread{
         private  BluetoothConnector bluetoothConnector;
         private BluetoothDevice bluetoothDevice;
@@ -287,10 +149,10 @@ public class ConnectedActivity extends ActionBarActivity implements SensorEventL
             bluetoothDevice = device;
             bluetoothAdapter = adpater;
             candidateUUIDs = UUIDs;
+            bluetoothConnector = new BluetoothConnector(bluetoothDevice, bluetoothAdapter, candidateUUIDs);
         }
 
         public void run(){
-            bluetoothConnector = new BluetoothConnector(bluetoothDevice, bluetoothAdapter, candidateUUIDs);
             try {
                 bluetoothConnector.connect();
                 Log.d(TAG, "Socket connected");
@@ -316,10 +178,12 @@ public class ConnectedActivity extends ActionBarActivity implements SensorEventL
 
     private class StreamToServer extends Thread{
         private OutputStream outStream;
+        private BluetoothConnector bluetoothConnector;
 
-        public StreamToServer(BluetoothConnector bluetoothConnector){
+        public StreamToServer(BluetoothConnector btConnector){
             try {
-                this.outStream = bluetoothConnector.getOutputStream();
+                bluetoothConnector = btConnector;
+                outStream = bluetoothConnector.getOutputStream();
             } catch (IOException e){
                 Log.d(TAG, "Unable to get output stream");
                 e.printStackTrace();
